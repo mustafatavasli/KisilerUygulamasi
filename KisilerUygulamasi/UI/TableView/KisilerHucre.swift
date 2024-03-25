17bd345d483a38d4abe2aad6269ef61a8bfd3f68
mustafatavasli <musstafatavasli@gmail.com>
2024-03-25 18:33:59 +0300

    TableView Added

---
+++ KisilerUygulamasi/UI/TableView/KisilerHucre.swift
@@ -0,0 +1,26 @@
+//
+//  KisilerHucre.swift
+//  KisilerUygulamasi
+//
+//  Created by Mustafa TAVASLI on 25.03.2024.
+//
+
+import UIKit
+
+class KisilerHucre: UITableViewCell {
+
+    @IBOutlet weak var lblKisiAdi: UILabel!
+    @IBOutlet weak var lblTelefonNumarasi: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- KisilerUygulamasi/UI/Views/KisiListe.swift
+++ KisilerUygulamasi/UI/Views/KisiListe.swift
@@ -9,14 +9,23 @@ import UIKit
 
 class KisiListe: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var kisilerTableView: UITableView!
+    
+    var kisilerListesi = [Kisiler]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func btnDetay(_ sender: Any) {
-        let kisi = Kisiler(id: 1, ad: "Mustafa", numara: "541 810 34 64")
-        performSegue(withIdentifier: "toKisiDetay", sender: kisi)
+        searchBar.delegate = self
+        kisilerTableView.delegate = self
+        kisilerTableView.dataSource = self
+        
+        let kisi1 = Kisiler(id: 1, ad: "Mustafa", numara: "541 810 34 64")
+        let kisi2 = Kisiler(id: 2, ad: "Zeynep", numara: "542 xxx xx xx")
+        let kisi3 = Kisiler(id: 3, ad: "Batuhan", numara: "536 xxx xx xx")
+        kisilerListesi.append(kisi1)
+        kisilerListesi.append(kisi2)
+        kisilerListesi.append(kisi3)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -27,6 +36,26 @@ class KisiListe: UIViewController {
             }
         }
     }
-    
 }
 
+extension KisiListe : UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Kişi Adı : \(searchText)")
+    }
+}
+
+extension KisiListe : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListesi[indexPath.row]
+        let hucre = tableView.dequeueReusableCell(withIdentifier: "kisilerHucre") as! KisilerHucre
+        
+        hucre.lblKisiAdi.text = kisi.ad
+        hucre.lblTelefonNumarasi.text = kisi.numara
+        
+        return hucre
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kisilerListesi.count
+    }
+}
